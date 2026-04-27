cc012c12eb4b33ff69a08c36a7640f4f2b71c03c
Farid <i.faridizzat@gmail.com>
2026-04-27 17:39:05 +0800

    Sprint 1: Role enum, User entity, config files

--- src/main/java/com/fittrack/FittrackBackendApplication.java
+++ src/main/java/com/fittrack/FittrackBackendApplication.java
@@ -1,4 +1,4 @@
-package com.fittrack.fittrack_backend;
+package com.fittrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FittrackBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FittrackBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FittrackBackendApplication.class, args);
+    }
 
 }
